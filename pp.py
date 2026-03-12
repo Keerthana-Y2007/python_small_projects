27d7a7736bafaf13256dbf6ad7c3f489a74aefd1
Keerthana Y <keerthana.y02007@gmail.com>
2026-03-12 22:08:00 +0530

    first commit

---
+++ pp.py
@@ -0,0 +1,7 @@
+radius=(int(input("enter radius")))
+area_of_circle=3.14*radius**2
+print(area_of_circle)
+name=input("hamsana")
+age=input("16")
+print(name +' is '+age+' years old')
+print("my age is",18)
---
+++ python.py
@@ -0,0 +1,31 @@
+print("hello  world ")
+print("-" * 50
+      )
+print("        BASICS OPERATORS")
+num1=float(input("enter first number\n"))
+num2=float(input("enter second number\n"))
+print("  \n Select the operation:\n")
+print("1.add")
+print("2.subract")
+print("3.multiplication")
+print("4.division")
+choice=int(input("\n enter 1-4\n"))
+if choice==1:
+  result = num1+num2  
+  print(f"{num1}+{num2}={result}")  
+elif choice==2:
+  result=num1-num2
+  print(f"{num1}-{num2}={result}")
+elif choice==3:
+   result=num1*num2
+   print(f"{num1}*{num2}={result}")
+   
+elif choice==4:
+    if num2==0:
+       print("it cannot be zero")
+    else:
+       result=num1/num2
+       print(f"{num1}/{num2}={result}")  
+else:
+     print("invalid") 
+print("-" * 50)      
